d3826f6fb96a9d333db69462acac3e86a4744305
pedro-rodriguez-1995 <rodriguezjimenez1995@gmail.com>
2022-03-01 20:13:49 +0100

    added features to download the csv files and read from them closes #2

--- src/main/java/com/sinensia/utils/CsvDownload.java
+++ src/main/java/com/sinensia/utils/CsvDownload.java
@@ -1,5 +1,30 @@
 package com.sinensia.utils;
 
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
 public class CsvDownload {
 
+	public static void download(String link, String filename) throws FileNotFoundException, IOException {
+		
+		try(BufferedInputStream in = new BufferedInputStream(new URL(link).openStream());
+				FileOutputStream fileOutputStream = new FileOutputStream(filename)){
+			
+			byte dataBuffer[] = new byte[1024];
+			int bytesRead;
+			while((bytesRead = in.read(dataBuffer,0,1024))!= -1) {
+				
+				fileOutputStream.write(dataBuffer,0,bytesRead);
+				
+				
+			}
+			
+			System.out.println(filename+" file downloaded.");
+		}
+		
+		
+	}
 }
