914ad15b5406262bc9b66d67136297a07fe3291b
pedro-rodriguez-1995 <rodriguezjimenez1995@gmail.com>
2022-03-02 21:05:52 +0100

    added the scheduler function closes #14

--- src/main/java/com/sinensia/principal/Principal.java
+++ src/main/java/com/sinensia/principal/Principal.java
@@ -3,28 +3,36 @@ package com.sinensia.principal;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.sinensia.logic.StockCheckerLauncher;
 
 public class Principal {
-	
 
 	public static void main(String[] args) {
-		
 
+		AnnotationConfigApplicationContext annotationContext = new AnnotationConfigApplicationContext();
+		annotationContext.scan("com.sinensia.logic");
+		annotationContext.refresh();
+
+		StockCheckerLauncher stockcheckerlauncher = annotationContext.getBean(StockCheckerLauncher.class);
 		try {
-
-			StockCheckerLauncher.launch();
-
+			stockcheckerlauncher.launch();
+			waitMethod();
 		} catch (IOException e) {
-
-			System.out.println("File not found");
-
-		} catch (InterruptedException | ExecutionException e) {
-
-			System.out.println("Thread error: " + e);
-
+			System.out.println("File not found: "+e);
+			e.printStackTrace();
+		} catch (InterruptedException  | ExecutionException e) {
+			System.out.println("Thread error: "+e);
+			e.printStackTrace();
+		} finally {
+			annotationContext.close();
 		}
 
 	}
 
+	public synchronized static void waitMethod() {
+		while (true) {
+		}
+	}
 }
